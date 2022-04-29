9b3bd03f94d2a07b1d39cdad25bfe859f0d97ea4
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2022-04-29 15:34:49 +0900

    초기화 작업후 배포

---
+++ src/main/java/com/finalproject/mohel/MohelApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.mohel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/mohel/MohelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.mohel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
