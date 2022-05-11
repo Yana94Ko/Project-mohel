0b0fdc00d9b3c98e91e6ff141ba71a003f87bd8a
hyeon3 <dldlsrn231@gmail.com>
2022-05-11 14:34:03 +0900

    update

--- src/main/java/com/finalproject/mohel/controller/MemberController.java
+++ src/main/java/com/finalproject/mohel/controller/MemberController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.finalproject.mohel.service.Certified;
