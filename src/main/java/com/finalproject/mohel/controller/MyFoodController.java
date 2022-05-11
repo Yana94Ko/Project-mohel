bce69f9231e0b1ba8c1d36aff15f5c5f396a5ad4
hyeon3 <dldlsrn231@gmail.com>
2022-05-11 18:38:52 +0900

    Gson 진짜 컷!!

--- src/main/java/com/finalproject/mohel/controller/MyFoodController.java
+++ src/main/java/com/finalproject/mohel/controller/MyFoodController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.finalproject.mohel.service.MyFoodService;
 import com.finalproject.mohel.vo.MyFoodVO;
-import com.google.gson.Gson;
 
 @Controller
 public class MyFoodController {
@@ -30,7 +29,6 @@ public class MyFoodController {
 		}
 		String dateParam = String.format("%d-%02d-%02d", current.getYear(), current.getMonthValue(), current.getDayOfMonth());
 		List<MyFoodVO> list = service.getMyFood(dateParam+"%", "ㅇㅇ");
-		
 		
 		HashMap<String, Integer> map = new HashMap<>();
 		map.put("year", current.getYear());
